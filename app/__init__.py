4e0cbff5a2fd9f9f742a20f9217f370e4d180673
TemitopeEmokpae <temokpae@oswego.edu>
2024-06-22 23:00:19 -0400

    testing this merge.

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,15 @@ hobbies = []
 
 @app.route("/")
 def index():
-    return render_template("index.html", title="MLH Fellow", url=os.getenv("URL"))
+    return render_template(
+        "index.html",
+        title="MLH Fellow",
+        url=os.getenv("URL"),
+        work_experiences=work_experiences,
+        educations=educations,
+        hobbies=hobbies,
+    )
+
 
 @app.route('/work', methods=['GET', 'POST'])
 def work():
@@ -39,6 +47,7 @@ def work():
         return redirect(url_for('work'))
     return render_template('work.html', title="Work and Education", work_experiences=work_experiences, educations=educations, url=os.getenv("URL"))
 
+
 @app.route("/hobbies", methods=["GET", "POST"])
 def hobbies_page():
     if request.method == "POST":
@@ -52,6 +61,46 @@ def hobbies_page():
         "hobbies.html", title="Hobbies", hobbies=hobbies, url=os.getenv("URL")
     )
 
+
 @app.route("/places")
 def places():
     return render_template("places.html", title="MLH Fellow", url=os.getenv("URL"))
+
+
+@app.route("/add_experience", methods=["POST"])
+def add_experience():
+    new_experience = {
+        "title": request.form["title"],
+        "company": request.form["company"],
+        "location": request.form["location"],
+        "duration": request.form["duration"],
+        "description": request.form["description"],
+    }
+    work_experiences.append(new_experience)
+    return redirect(url_for("index"))
+
+
+@app.route("/add_education", methods=["POST"])
+def add_education():
+    new_education = {
+        "degree": request.form["degree"],
+        "university": request.form["university"],
+        "location": request.form["location"],
+        "duration": request.form["duration"],
+        "description": request.form["description"],
+    }
+    educations.append(new_education)
+    return redirect(url_for("index"))
+
+
+@app.route("/add_hobby", methods=["POST"])
+def add_hobby():
+    new_hobby = {
+        "name": request.form["name"],
+        "description": request.form["description"],
+    }
+    hobbies.append(new_hobby)
+    return redirect(url_for("index"))
+
+if __name__ == "__main__":
+    app.run(debug=True)
